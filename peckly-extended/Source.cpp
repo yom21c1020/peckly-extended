4134f71d5a40ee5603550fe232d61c29c4988920
Yusi <yusi@kafu.moe>
2018-02-23 13:25:49 +0900

    asdf

--- peckly-extended/Source.cpp
+++ peckly-extended/Source.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
 #include <algorithm>
+#include <random>
+#include <ctime>
 using namespace std;
-int d[100001], c[100001], x[5][100001];
-int k0, k1, n, j;
+int d[100001], c[100001], x[5][100001], f[100001];
+int k0, k1, n, j, m;
+int test(int n);
 //////////////////////////////////////
 void A(int x) {
 	int i;
 	for (i = 1; i <= x; i++) {
 		swap(d[i], d[i + x]);
 	}
+	//////////////////////////////////
+	cout << "A(" << x << ") : ";
+	for (i = 1; i <= n; i++) {
+		cout << d[i] << " ";
+	}
+	cout << endl;
 }
 void B() {
 	swap(d[1], d[n / 2 + 1]);
-	swap(d[1], d[n / 2 + 1]);
+	//////////////////////////////////
+	cout << "B() : ";
+	for (int i = 1; i <= n; i++) {
+		cout << d[i] << " ";
+	}
+	cout << endl;
 }
 //////////////////////////////////////
 void ex_hunt(int p)
 {
-	int k;
 	if (p == 0) j = k0;
 	else j = k1;
 	if (j == 0) return;
@@ -30,23 +43,23 @@ void ex_hunt(int p)
 //////////////////////////////////////
 void hunt(int alpha, int beta, int gamma) {
 	int k = 0;
-	if (beta != 1) {
-		if (beta == 2) goto hunt_end2;
-	hunt_restart:
-		if ((2 * alpha - 1) / beta == 1) {
-			A(beta / 2);
-			x[gamma][k] = beta / 2;
-			k++;
-		}
-		beta /= 2;
-		alpha %= beta;
-		if (beta == 2) goto hunt_end2;
-		else goto hunt_restart;
+	if (beta == 1) goto hunt_end;
+	if (beta == 2) goto hunt_end2;
+hunt_restart:
+	if (((2 * alpha - 1) / beta) == 1) {
+		A(beta / 2);
+		x[gamma][k] = beta / 2;
+		k++;
 	}
+	beta /= 2;
+	alpha % beta == 0 ? alpha = alpha : alpha %= beta;
+	if (beta == 2) goto hunt_end2;
+	else goto hunt_restart;
+
 hunt_end2:
 	if (alpha != 1) {
 		A(1);
-		x[gamma][k] = 1;
+		x[gamma][k] = 1;	
 		k++;
 	}
 hunt_end:
@@ -78,60 +91,88 @@ void xbox(int a, int b)
 		ex_hunt(0);
 		goto asdf;
 	}
-	else if (a > (n / 2) && b > (n / 2)) {
+	else if (a > (m / 2) && b > (m / 2)) {
 		A(n / 2);
-		a -= (n / 2);
-		b -= (n / 2);
-		hunt(a, n / 2, 0);
+		hunt(a - (n / 2), n / 2, 0);
 		B();
-		hunt(b, n / 2, 1);
-		B();
-		ex_hunt(1);
+		hunt(b - (n / 2), n / 2, 1);
 		B();
 		ex_hunt(1);
 		B();
 		ex_hunt(0);
-		a += (n / 2);
-		b += (n / 2);
 		A(n / 2);
 		goto asdf;
 	}
 	else {
 		hunt(a, n / 2, 0);
 		A(n / 2);
-		b -= n / 2;
-		hunt(b, n / 2, 1);
+		hunt(b - (n / 2), n / 2, 1);
 		B();
 		ex_hunt(1);
-		A(n / 2);
+		A(m / 2);
 		ex_hunt(0);
-		b += n / 2;
 		goto asdf;
 	}
 asdf:
 	swap(c[d[a]], c[d[b]]);
+	swap(f[a], f[b]);
+	cout << "//////////////////////////////////////" << endl;
 	return;
 }
 //////////////////////////////////////
 int main()
 {
 	int i;
-	cin >> n;
+	cin >> m;
+	test(m);
 
+	while (m < n) n *= 2;
 	i = 1;
-a:
-	c[d[i]] = i;
-	if (i < n) {
+	while (i < n) {
+		f[i] = 1;
 		i++;
-		goto a;
+	}
+	i = m + 1;
+	while (i < n) {
+		d[i] = INT_MAX;
+		i++;
 	}
 
-	i = n;
-b:
-	xbox(c[i], i);
-	if (i > 1) {
-		i--;
-		goto b;
+	i = 1;
+main_b:
+	j = i + 1;
+main_a:
+	if (d[i] > d[j]) d[i]++;
+	else d[j]++;
+	j++;
+	if (j <= n) goto main_a;
+	if (i <= n) goto main_b;
+	for (i = 1; i < m; i++) c[f[i]] = i;
+	for (i = m; i > 1; i--) xbox(c[i], i);
+
+	cout << endl;
+	for (i = 1; i <= n; i++) {
+		cout << d[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
+//////////////////////////////////////
+int test(int n)
+{
+	int is_test, i;
+	cin >> is_test;
+	if (!is_test) {
+		for (i = 1; i <= n; i++) {
+			cin >> d[i];
+		}
+	}
+	else {
+		std::default_random_engine generator;
+		std::uniform_int_distribution<int> distribution(1, 10);
+		for (i = 1; i <= n; i++) {
+			d[i] = distribution(generator);
+		}
 	}
 	return 0;
 }
