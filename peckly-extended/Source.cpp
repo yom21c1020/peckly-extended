6a1d4934d091cb46c2c22900711d6d64893a9d9b
Yusi <yusi@kafu.moe>
2018-07-25 17:08:00 +0900

    자리배열 알고리즘 필요

--- peckly-extended/Source.cpp
+++ peckly-extended/Source.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <ctime>
 using namespace std;
-int d[100001], c[100001], x[5][100001], f[100001];
+int d[100001];
 int k0, k1, n, j, m;
 int test(int n);
 //////////////////////////////////////
@@ -13,166 +13,140 @@ void A(int x) {
 		swap(d[i], d[i + x]);
 	}
 	//////////////////////////////////
-	cout << "A(" << x << ") : ";
+	cout << "    A(" << x << ") : ";
 	for (i = 1; i <= n; i++) {
 		cout << d[i] << " ";
 	}
 	cout << endl;
 }
-void B() {
-	swap(d[1], d[n / 2 + 1]);
+void B(int x) {
+	swap(d[1], d[x / 2 + 1]);
 	//////////////////////////////////
-	cout << "B() : ";
+	cout << "    B(" << x << ") : ";
 	for (int i = 1; i <= n; i++) {
 		cout << d[i] << " ";
 	}
 	cout << endl;
 }
 //////////////////////////////////////
-void ex_hunt(int p)
+int max(int n, int m)
 {
-	if (p == 0) j = k0;
-	else j = k1;
-	if (j == 0) return;
-	while (1) {
-		j--;
-		A(x[p][j]);
-		if (j <= 0) return;
+	int i, maxidx, max = INT_MIN;
+	for (i = n; i <= m; i++) {
+		if (max < d[i]) {
+			max = d[i];
+			maxidx = i;
+		}
 	}
+	return maxidx;
+}
+bool chkSrt()
+{
+	int i, prev = d[1];
+	for (i = 2; i <= n; i++) {
+		if (prev > d[i]) return 0;
+		prev = d[i];
+	}
+	return 1;
 }
 //////////////////////////////////////
-void hunt(int alpha, int beta, int gamma) {
-	int k = 0;
-	if (beta == 1) goto hunt_end;
-	if (beta == 2) goto hunt_end2;
-hunt_restart:
-	if (((2 * alpha - 1) / beta) == 1) {
-		A(beta / 2);
-		x[gamma][k] = beta / 2;
-		k++;
-	}
-	beta /= 2;
-	alpha % beta == 0 ? alpha = alpha : alpha %= beta;
-	if (beta == 2) goto hunt_end2;
-	else goto hunt_restart;
 
-hunt_end2:
-	if (alpha != 1) {
-		A(1);
-		x[gamma][k] = 1;	
-		k++;
+void s1()
+{
+	int i, max, maxidx;
+	max = d[1];
+	maxidx = 1;
+	for (i = 1; i <= n / 2; i++) {
+		if (max < d[i]) {
+			max = d[i];
+			maxidx = i;
+		}
 	}
-hunt_end:
-	if (gamma == 1)	k1 = k;
-	else k0 = k;
+	int curN = n / 2;
+	while (maxidx != 1) {
+		if (maxidx > curN / 2) {
+			A(curN / 2);
+			maxidx -= curN / 2;
+		}
+		curN /= 2;
+	}
+
+	if (chkSrt()) 
+		exit(0);
+	return;
 }
 //////////////////////////////////////
-void xbox(int a, int b)
+void s2()
 {
-	if (a == b) return;
-	if (a > b) {
-		swap(a, b);
+	int i, cnt1, cnt2;
+	cnt1 = cnt2 = 0;
+	for (i = 1; i <= n / 2; i++) cnt1 += d[i];
+	for (i = n / 2 + 1; i <= n; i++) cnt2 += d[i];
+	if (cnt1 < cnt2) A(n / 2);
+	if (chkSrt()) 
+		exit(0);
+	return;
+}
+void s2_R()
+{
+	int i, cnt1, cnt2;
+	cnt1 = cnt2 = 0;
+	for (i = 1; i <= n / 2; i++) cnt1 += d[i];
+	for (i = n / 2 + 1; i <= n; i++) cnt2 += d[i];
+	if (cnt1 > cnt2) A(n / 2);
+	if (chkSrt()) 
+		exit(0);
+	return;
+}
+//////////////////////////////////////
+void s3()
+{
+	int i, min, minidx;
+	min = d[1];
+	minidx = 1;
+	for (i = 1; i <= n / 2; i++) {
+		if (min > d[i]) {
+			min = d[i];
+			minidx = i;
+		}
 	}
-	if (a == 1 && b == 2) {
-		A(1);
-		goto asdf;
+	int curN = n / 2;
+	while (minidx != 1) {
+		if (minidx > curN / 2) {
+			A(curN / 2);
+			minidx -= curN / 2;
+		}
+		curN /= 2;
 	}
-	if (a == 1 && b == (n / 2) + 1) {
-		B();
-		goto asdf;
-	}
-	if (a <= (n / 2) && b <= (n / 2)) {
-		hunt(a, n / 2, 0);
-		B();
-		hunt(b, n / 2, 1);
-		B();
-		ex_hunt(1);
-		B();
-		ex_hunt(0);
-		goto asdf;
-	}
-	else if (a > (m / 2) && b > (m / 2)) {
-		A(n / 2);
-		hunt(a - (n / 2), n / 2, 0);
-		B();
-		hunt(b - (n / 2), n / 2, 1);
-		B();
-		ex_hunt(1);
-		B();
-		ex_hunt(0);
-		A(n / 2);
-		goto asdf;
-	}
-	else {
-		hunt(a, n / 2, 0);
-		A(n / 2);
-		hunt(b - (n / 2), n / 2, 1);
-		B();
-		ex_hunt(1);
-		A(m / 2);
-		ex_hunt(0);
-		goto asdf;
-	}
-asdf:
-	swap(c[d[a]], c[d[b]]);
-	swap(f[a], f[b]);
-	cout << "//////////////////////////////////////" << endl;
+
+	if (chkSrt()) 
+		exit(0);
 	return;
 }
 //////////////////////////////////////
 int main()
 {
 	int i;
-	cin >> m;
-	test(m);
-
-	while (m < n) n *= 2;
-	i = 1;
-	while (i < n) {
-		f[i] = 1;
-		i++;
+	cin >> n;
+	for (i = 1; i <= n; i++) cin >> d[i];
+	while (1) {
+		cout << "St.1" << endl;
+		s1();
+		cout << "St.2" << endl;
+		s2();
+		cout << "St.3" << endl;
+		s3();
+		cout << "St.4" << endl;
+		B(n);
+		cout << "St.1_R" << endl;
+		s3();
+		cout << "St.2_R" << endl;
+		s2_R();
+		cout << "St.3_R" << endl;
+		s1();
+		cout << "St.4_R" << endl;
+		B(n);
 	}
-	i = m + 1;
-	while (i < n) {
-		d[i] = INT_MAX;
-		i++;
-	}
-
-	i = 1;
-main_b:
-	j = i + 1;
-main_a:
-	if (d[i] > d[j]) d[i]++;
-	else d[j]++;
-	j++;
-	if (j <= n) goto main_a;
-	if (i <= n) goto main_b;
-	for (i = 1; i < m; i++) c[f[i]] = i;
-	for (i = m; i > 1; i--) xbox(c[i], i);
-
-	cout << endl;
-	for (i = 1; i <= n; i++) {
-		cout << d[i] << " ";
-	}
-	cout << endl;
-	return 0;
-}
-//////////////////////////////////////
-int test(int n)
-{
-	int is_test, i;
-	cin >> is_test;
-	if (!is_test) {
-		for (i = 1; i <= n; i++) {
-			cin >> d[i];
-		}
-	}
-	else {
-		std::default_random_engine generator;
-		std::uniform_int_distribution<int> distribution(1, 10);
-		for (i = 1; i <= n; i++) {
-			d[i] = distribution(generator);
-		}
-	}
+	
 	return 0;
 }
